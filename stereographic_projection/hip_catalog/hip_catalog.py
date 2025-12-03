3f176360dc4fc9689ccdbfd05339cde7440343f6
Maksimka <138521714+04Maksimka@users.noreply.github.com>
2025-12-03 21:50:53 +0300

    small refactor

--- stereographic_projection/hip_catalog/hip_catalog.py
+++ stereographic_projection/hip_catalog/hip_catalog.py
@@ -5,17 +5,17 @@ from numpy.typing import NDArray
 
 
 @dataclass
-class Catalog(object):
-    """Hipparchus catalog."""
-    mag_criteria: float = 5.5
-
-    def get_data(self):
-        """Returns list of stars with given constrains."""
-        raise NotImplemented
-
-
-@dataclass
 class Star(object):
     """Class of a singular star."""
     v_mag: float
     eci_coords: NDArray
+
+
+@dataclass
+class Catalog(object):
+    """Hipparchus catalog."""
+    mag_criteria: float = 5.5
+
+    def get_data(self) -> NDArray[Star]:
+        """Returns list of stars with given constrains."""
+        raise NotImplemented
--- stereographic_projection/stereographic_projector.py
+++ stereographic_projection/stereographic_projector.py
@@ -1,19 +1,26 @@
 """Module implementing main stereographic projection."""
 from dataclasses import dataclass
+from datetime import datetime
+
+from stereographic_projection.hip_catalog.hip_catalog import Catalog
 
 
 @dataclass
 class StereoProjCfg(object):
     """Class of configuration of the StereoProjector."""
+
     add_ecliptic: bool
+    utc_time: datetime
+    latitude: float
+    longitude: float
 
 
 class StereoProjector(object):
     """Class of the stereographic projector."""
 
-    def __init__(self, zenith_coords):
+    def __init__(self, cfg: StereoProjCfg, catalog: Catalog):
         raise NotImplemented
 
-    def generate(self):
+    def generate(self) -> None:
         """Generate a projection."""
         raise NotImplemented
