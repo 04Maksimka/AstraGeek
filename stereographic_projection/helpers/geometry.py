Fixed rotation matrix. Verified calculations with Stellarium. Sidereal time is correct on a scale of hundreds of years

stereographic_projection/helpers/geometry.py
@@ -43,9 +43,9 @@ def get_horizontal_coords(config: dict, catalog_data: NDArray[Star]):
     :param config: place configuration
     :param catalog_data: star catalog data
     :return: horizontal coordinates, zenith distances and azimuths
-
-    TODO: implement accurate sidereal time calculation
     """
+
+    # Get ECI star coordinates
     eci_coords = np.array([np.array(list(star.eci_coords)) for star in catalog_data]).T
 
     # Calculate local sidereal time
@@ -57,19 +57,21 @@ def get_horizontal_coords(config: dict, catalog_data: NDArray[Star]):
     # Rotate ECI (XYZ) to "cartesian" equatorial system (X'Y'Z'),
     # so Z' is directed to the North celestial pole, X' --- to the West point
     latitude = config['latitude']
+    sl = np.sin(latitude)
+    cl = np.cos(latitude)
+    st = np.sin(sidereal_time)
+    ct = np.cos(sidereal_time)
     rotation_matrix = np.array(
         [
-            [np.sin(sidereal_time), np.sin(latitude) * np.cos(sidereal_time),
-             -np.cos(latitude) * np.cos(sidereal_time)],
-            [-np.cos(sidereal_time), np.sin(latitude) * np.sin(sidereal_time),
-             -np.cos(latitude) * np.sin(sidereal_time)],
-            [0.0, np.cos(latitude), np.sin(latitude)]
+            [st, -ct, 0],
+            [sl * ct, sl * st, -cl],
+            [cl * ct, cl * st, sl]
         ]
     )
     cartesian_hor_coords = rotation_matrix @ eci_coords
 
     # 0, 1, 2 is x, y, z below
-    azimuths = -np.atan2(cartesian_hor_coords[1, :], cartesian_hor_coords[0, :])
+    azimuths = np.atan2(cartesian_hor_coords[0, :], cartesian_hor_coords[1, :]) - np.pi / 2
     zeniths = np.arccos(cartesian_hor_coords[2, :])
 
     return cartesian_hor_coords, azimuths, zeniths
@@ -82,7 +84,7 @@ def mag_to_radius(magnitude: float, mag_criteria: float) -> float:
     :param mag_criteria: max radius criteria
     :return: radius: star image radius
     """
-    return np.max([mag_criteria - magnitude, 0])
+    return 1.5*np.max([mag_criteria - magnitude, 0])
 
 
 def make_point_projections(star_view_data: NDArray[StarView], mag_criteria: float) -> NDArray[PointProjection]:

stereographic_projection/helpers/time.py
@@ -9,18 +9,18 @@ def get_sidereal_time(longitude: float, local: datetime):
     Calculate local sidereal time
 
     :param longitude: place longitude in radians
-    :param local: LMT
-    :return: LST
+    :param local: local time
+    :return: LST (Local Sidereal Time)
     """
 
     # Shift from UTC
     timeshift = get_timeshift(longitude)
-    utc = local - timeshift
-    year = utc.year
+    gmt = local
 
     # From 0 January
+    year = gmt.year
     origin = datetime(year, 1, 1, 0, 0, 0) - timedelta(days=1)
-    shift = int((utc - origin).total_seconds() / 86400.0)
+    shift = int((gmt - origin).total_seconds() / 86400.0)
 
     # Calculate julian date on 0 January
     jd = julian_date(origin)
@@ -33,23 +33,42 @@ def get_sidereal_time(longitude: float, local: datetime):
     C = 1.002738
     T0 = shift * A - B
 
-    LST = (C * (get_total_hours(local.time())) + T0) % 24
-    time_LST = get_time(LST)
+    lst = (C * (get_total_hours(local.time())) + T0) % 24
+    time_lst = get_time(lst)
 
-    return time_LST
+    return time_lst
 
 
-def vequinox_hour_angle(longitude: float, local: datetime):
+def vequinox_hour_angle(longitude: float, local: datetime) -> float:
+    """
+    Calculate vernal equinox hour angle
+    :param longitude: place longitude in radians
+    :param local: local time
+    :return: hour angle in radians
+    """
+
     sidereal_time = get_sidereal_time(longitude, local)
-    t = get_total_hours(sidereal_time) * 15.0
+    t = np.deg2rad(get_total_hours(sidereal_time) * 15.0)
     return t
 
 
 def get_total_hours(t: time) -> float:
+    """
+    Calculate time in hours (with floating point)
+    :param t: time hh:mm:ss
+    :return: hours
+    """
+
     return t.hour + t.minute / 60.0 + t.second / 3600.0
 
 
 def get_timeshift(longitude: float) -> timedelta:
+    """
+    Calculate timeshift between local time and UTC
+    :param longitude: longitude in radians
+    :return: shift
+    """
+
     total_hours = np.rad2deg(longitude) / 15.0
     hours = int(total_hours % 24)
     minutes = int((total_hours - hours) * 60)
@@ -58,6 +77,12 @@ def get_timeshift(longitude: float) -> timedelta:
 
 
 def get_time(total_hours: float) -> time:
+    """
+    Calculate time object from total hours (with floating point)
+    :param total_hours: hours with floating point
+    :return: corresponding time
+    """
+
     hours = int(total_hours % 24)
     minutes = int((total_hours - hours) * 60)
     seconds = int((total_hours - hours - minutes / 60.0) * 3600)
@@ -68,6 +93,8 @@ def get_time(total_hours: float) -> time:
 def julian_date(date_time: datetime):
     """
     Returns the Julian date, number of days since 1 January 4713 BC 12:00 UTC.
+    :param date_time: datetime object
+    :return: Julian date
     """
 
     year = date_time.year

stereographic_projection/helpers/time_example.py
@@ -1,10 +1,10 @@
-from stereographic_projection.helpers.time import get_sidereal_time
+from stereographic_projection.helpers.time import get_sidereal_time, julian_date
 from datetime import datetime
 import numpy as np
 
 print(
     get_sidereal_time(
-        longitude=np.deg2rad(90),
+        longitude=np.deg2rad(0),
         local=datetime(
             year=1980,
             month=4,
@@ -15,4 +15,12 @@ print(
         )
     )
 )
+
+print(julian_date(
+    date_time=datetime(
+        year=2025,
+        month=12,
+        day=12,
+    )
+))
 

stereographic_projection/stereographic_projector.py
@@ -33,6 +33,7 @@ class StereoProjector(object):
     def generate(self) -> plt.Figure:
         """
         Generate a projection.
+        :return: figure
         """
 
         # Get catalog
@@ -53,6 +54,7 @@ class StereoProjector(object):
         :param catalog_data: catalog of the stars
         :return: star view parameters
         """
+
         _, azimuths, zeniths = get_horizontal_coords(self.config.__dict__, catalog_data=catalog_data)
 
         magnitudes = np.array([star.v_mag for star in catalog_data])
@@ -75,7 +77,11 @@ class StereoProjector(object):
 
     @staticmethod
     def _create_polar_scatter(data: NDArray[PointProjection]):
-        """Create a scatter plot in polar coordinates."""
+        """
+        Create a scatter plot in polar coordinates.
+        :param data: projection points to place on figure
+        """
+
         # Set up the figure with polar projection
         fig = plt.figure(figsize=(15, 12))
         ax = fig.add_subplot(111, projection='polar')
@@ -98,7 +104,7 @@ class StereoProjector(object):
 
         ax.set_title("Skychart", va='bottom', fontsize=14, pad=20)
         ax.set_xlabel("Angle (θ)", labelpad=15)
-        ax.grid(False)
+        ax.grid(True)
         # ax.set_xticks([])
         ax.set_yticks([])
         ax.set_ylim((0.0, 2.0))